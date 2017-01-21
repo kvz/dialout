277981d889be4a2e9cebf851140269184c4a5002
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-01-21 13:44:13 +0100

    Add ip

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const publicIp = require('public-ip')
+
+publicIp.v4().then(ip => {
+  document.write(ip)
+  console.log({ip})
+});
